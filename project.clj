3be5d4538f7959204858dc362585a98e0c76e201
Hugo Duncan <hugo@palletops.com>
2014-07-13 14:20:07 +0000

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/leinout "0.1.1"
+(defproject com.palletops/leinout "0.1.2-SNAPSHOT"
   :description "A library for doing things with git, github, travis and lein."
   :url "https://github.com/palletops/leinout"
   :license {:name "Eclipse Public License"
